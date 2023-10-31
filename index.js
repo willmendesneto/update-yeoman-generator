8a26caf02f2dcb3f6c166493051663bc9079cbde
Will Mendes <willmendesneto@gmail.com>
2023-10-31 19:52:13 -0300

    feat: adding new `json-storage-file` param to define json file (#67)

--- index.js
+++ index.js
@@ -3,22 +3,25 @@ const run = require('./lib/run');
 const chalk = require('chalk');
 const upath = require('upath');
 const { fileExists, printMessage } = require('./lib/util');
-const {
-  DEFAULT_EJS_OPEN,
-  DEFAULT_EJS_CLOSE,
-  DEFAULT_TEMPLATES_FOLDER,
-} = require('./lib/constants');
+const { DEFAULT_EJS_OPEN, DEFAULT_EJS_CLOSE, DEFAULT_TEMPLATES_FOLDER, DEFAULT_JSON_STORAGE_FILE } = require('./lib/constants');
 
 const { argv } = require('yargs')
   .describe('generator', 'Name of the Github generator. It should be in format `<github-user>/<github-repository>`')
-  .describe('template', `Optional: String with a prefix for your templates folder based on the root folder of the generator repository. Default: "${DEFAULT_TEMPLATES_FOLDER}"
+  .describe(
+    'template',
+    `Optional: String with a prefix for your templates folder based on the root folder of the generator repository. Default: "${DEFAULT_TEMPLATES_FOLDER}"
 
 EX:
 
     update-yeoman-generator --generator willmendesneto/generate-poi --template-prefix app/templates
 
     update-yeoman-generator --generator <github-user>/<github-repository> --github-token <your-github-token> --template-prefix app/templates
-  `)
+  `
+  )
+  .describe(
+    'json-storage-file',
+    `Optional: JSON file based on the root folder of the generator repository with the storage data. Default: "${DEFAULT_JSON_STORAGE_FILE}"`
+  )
   .describe('github-token', 'Optional: Github Token required for private repositories.')
   .describe('ejs-open', `Optional: '.ejs' File delimiter for open tag. Default: "${DEFAULT_TEMPLATES_FOLDER}"`)
   .describe('ejs-close', `Optional: '.ejs' File delimiter for close tag. Default: "${DEFAULT_TEMPLATES_FOLDER}"`)
@@ -36,12 +39,13 @@ printMessage(chalk.inverse(' INFO ') + ' Starting `update-yeoman-generator` scri
 const {
   generator,
   githubToken,
+  jsonFile = DEFAULT_JSON_STORAGE_FILE,
   template = DEFAULT_TEMPLATES_FOLDER,
   ejsOpen = DEFAULT_EJS_OPEN,
   ejsClose = DEFAULT_EJS_CLOSE,
 } = argv;
 
-const yoRcJsonPath = upath.join(process.cwd(), '.yo-rc.json');
+const yoRcJsonPath = upath.join(process.cwd(), jsonFile);
 
 if (!fileExists(yoRcJsonPath)) {
   throw new Error(`
